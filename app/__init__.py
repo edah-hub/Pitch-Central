8b6f4a8e70de715ef86fc0968c56de240e583891
edah-hub <edah.chepngetich@student.moringaschool.comm>
2022-05-08 18:16:11 +0300

    create_app application factory function we then call the init_appmethod and pass in the app instance

--- app/__init__.py
+++ app/__init__.py
@@ -19,6 +19,7 @@ def create_app(config_name):
     
         # Initializing flask extensions
     bootstrap.init_app(app)
+    db.init_app(app)
     
         # Registering the blueprint
     from .main import main as main_blueprint
