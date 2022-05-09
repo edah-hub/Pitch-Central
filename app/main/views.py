d8c26c3b0480e14aab34e0c18048acb588a8f232
edah-hub <edah.chepngetich@student.moringaschool.comm>
2022-05-09 06:38:29 +0300

    adds abort function that stops request

--- app/main/views.py
+++ app/main/views.py
@@ -1,4 +1,6 @@
-from flask import render_template 
+from flask import render_template,request,redirect,url_for,abort
+from ..models import User
+
 from . import main
 from flask_login import login_required
 # from app.models import Pitch
@@ -14,4 +16,13 @@ def index():
   # pitches = Pitch.query.all()
 
   return render_template('index.html', title=title)
+
+@main.route('/user/<uname>')
+def profile(uname):
+    user = User.query.filter_by(username = uname).first()
+
+    if user is None:
+        abort(404)
+
+    return render_template("profile/profile.html", user = user)
 
