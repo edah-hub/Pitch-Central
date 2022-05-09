a59d76da7c00a5ff6da6b325c49c24af8a85b3d3
edah-hub <edah.chepngetich@student.moringaschool.comm>
2022-05-09 06:01:25 +0300

    creates login form

--- app/models.py
+++ app/models.py
@@ -15,7 +15,7 @@ class User(db.Model, UserMixin):
   __tablename__ = 'users'
 
   id = db.Column(db.Integer, primary_key = True)
-  username = db.Column(db.String(20), unique=True, nullable =False)
+  username = db.Column(db.String(200), unique=True, nullable =False)
   email = db.Column(db.String(120), unique=True, nullable =False)
   image_file = db.Column(db.String(120), nullable =False, default='default.jpg')
   password = db.Column(db.String(60), nullable =False)
